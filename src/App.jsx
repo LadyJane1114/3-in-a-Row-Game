da615a7b6bea04898144a1c0c1eb54dc0269aeec
LadyJane1114 <sweet.jane.s@outlook.com>
2025-03-31 22:18:12 -0300

    navbar planning - not yet implemented but will edit when the appropriate paths are created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import { Link, Routes, Route} from 'react-router-dom'
 import './App.css'
+import TopNav from './Components/TopNav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Game/>
+      <TopNav/>
+      {/* <Game/> */}
 
       {/* <Routes>
         <Route path="/random" element={<Random/>}/>
---
+++ src/Components/TopNav.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+import { Nav, Navbar, Container } from "react-bootstrap"
+
+const TopNav = () => {
+  return (
+    <>
+        <Navbar bg="warning" data-bs-theme="dark">
+            <Container>
+                {/* <Navbar.Brand as={Link} to="/">Three In a Row</Navbar.Brand> */}
+                <Navbar.Brand>Three In a Row</Navbar.Brand>
+                <Nav className="me-auto">
+                    {/* <Nav.Link as={Link} to="/sample">Sample</Nav.Link>
+                    <Nav.Link as={Link} to="/random">Random</Nav.Link> */}
+                    <li>Sample</li>
+                    <li>Random</li>
+                </Nav>
+            </Container>
+        </Navbar>
+        </>
+  )
+}
+
+export default TopNav
